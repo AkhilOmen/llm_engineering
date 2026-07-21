0126842a2a71d88a8511df34c515db5874b3959c
AkhilOmen <akhilbv333@gmail.com>
2026-07-21 10:12:31 +0530

    DAY 2: open source and frontier close soruce LLMs, Ollama > local LLMs

--- projects/1_project/analyse_website_main.py
+++ projects/1_project/analyse_website_main.py
@@ -1,5 +1,5 @@
-from generic_utils import fetch_website_content
-from openai_utils import openai_analyze_text
+from scrapper import fetch_website_content
+from utils.openai_utils import openai_text_completion
 
 
 if __name__ == '__main__':
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     website_text: str = fetch_website_content(
         url="https://www.amazon.in/Voltas-Vectra-Elite-Cooling-cools-Anti-microbial/dp/B0BBFTT3YL/"
     )
-    openai_analyze_text(text=website_text)
+    openai_text_completion(text=website_text)
 
     pass
