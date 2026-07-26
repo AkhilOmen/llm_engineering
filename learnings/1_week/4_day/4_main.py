4e59363d3c706ebd15b577b0feab4786982aa1e7
AkhilOmen <akhilbv333@gmail.com>
2026-07-26 16:19:10 +0530

    Parameters, Tokens, Tokenizers, Memory Illusion, Context window, and Pricing

---
+++ learnings/1_week/4_day/4_main.py
@@ -0,0 +1,18 @@
+
+import tiktoken
+
+from utils.openai_utils import llm_is_stateless
+
+if __name__ == '__main__':
+    # encoding = tiktoken.encoding_for_model("gpt-4.1-mini")
+    # # encoding = tiktoken.encoding_for_model("gpt-5")
+    # tokens = encoding.encode("Hi my name is Akhil. I like chicken biryani a lot.")
+    #
+    # for token_id in tokens:
+    #     word = encoding.decode([token_id])
+    #     print(f"{token_id}={word}")
+    #
+    # # 12194=Hi
+    # # 13232= Ak
+
+    llm_is_stateless()
--- utils/openai_utils.py
+++ utils/openai_utils.py
@@ -31,3 +31,46 @@ def openai_text_completion(text):
     openai_response = openai.chat.completions.create(model="gpt-4.1-nano", messages=messages)
     parsed_response = openai_response.choices[0].message.content
     print(parsed_response)
+
+
+def llm_is_stateless():
+    openai = create_openai_client()
+
+    # Case 1
+    messages = [
+        {"role": "system", "content": "you are a helpful assistant"},
+        {"role": "user", "content": "Hi my name is Akhil"}
+    ]
+    openai_response = openai.chat.completions.create(model="gpt-4.1-nano", messages=messages)
+    parsed_response = openai_response.choices[0].message.content
+    print(parsed_response)
+
+    # response
+    # Hi Akhil, How may I assist you.
+
+
+    # Case 2
+    messages = [
+        {"role": "system", "content": "you are a helpful assistant"},
+        {"role": "user", "content": "What's my name"}
+    ]
+    openai_response = openai.chat.completions.create(model="gpt-4.1-nano", messages=messages)
+    parsed_response = openai_response.choices[0].message.content
+    print(parsed_response)
+
+    # response
+    # I don't have access to you personal information, how may I assist you.
+
+    # Case 3
+    messages = [
+        {"role": "system", "content": "you are a helpful assistant"},
+        {"role": "user", "content": "Hi my name is Akhil"},
+        {"role": "assistant", "content": "Hi Akhil, How may I help you today."},
+        {"role": "user", "content": "What's my name"}
+    ]
+    openai_response = openai.chat.completions.create(model="gpt-4.1-nano", messages=messages)
+    parsed_response = openai_response.choices[0].message.content
+    print(parsed_response)
+
+    # response
+    # Your name is Akhil! How can I help you today
